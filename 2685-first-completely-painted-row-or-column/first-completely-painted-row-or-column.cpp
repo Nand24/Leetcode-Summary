0737cdf6d6983e44a042e4dd1b11d368f62ec6b6
Pranav Sharma <120562457+Nand24@users.noreply.github.com>
2025-01-20 19:57:51 +0530

    Time: 111 ms (35.37%) | Memory: 171 MB (28.35%) - LeetSync

---
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        unordered_map<int, pair<int, int>> positions;
+        for (int i = 0; i < mat.size(); i++) {
+            for (int j = 0; j < mat[i].size(); j++) {
+                positions[mat[i][j]] = {i, j};
+            }
+        }
+        int colcnt = mat[0].size();
+        int rowcnt = mat.size();
+        vector<int> rowfreq(rowcnt, 0);
+        vector<int> colfreq(colcnt, 0);
+        for (int i = 0; i < arr.size(); i++) {
+            int val = arr[i];
+           int row= positions[val].first ;
+           int col= positions[val].second ;
+            ++rowfreq[row];
+            ++colfreq[col];
+            if(rowfreq[row]== colcnt || colfreq[col]==rowcnt)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
