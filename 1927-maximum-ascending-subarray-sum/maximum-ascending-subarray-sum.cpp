01405f0b29c0d2c231bd71c9e67d3ce1262f45cc
Pranav Sharma <120562457+Nand24@users.noreply.github.com>
2025-02-04 18:03:54 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (47.21%) - LeetSync

--- 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.cpp
+++ 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.cpp
@@ -7,19 +7,16 @@ public:
         int currentsum = nums[0];
         for (int i = 1; i < nums.size(); i++) {
 
-           
-                if (nums[i] > nums[i-1]) {
+            if (nums[i] > nums[i - 1]) {
 
-                  
-                    currentsum += nums[i];maxsum = max(maxsum, currentsum);
-                } else {
-                    maxsum = max(maxsum, currentsum);
-                    currentsum = nums[i];
-                }
+                currentsum += nums[i];
+                maxsum = max(maxsum, currentsum);
+            } else {
+                maxsum = max(maxsum, currentsum);
+                currentsum = nums[i];
             }
+        }
 
         return maxsum;
-        }
-        
-    
+    }
 };
