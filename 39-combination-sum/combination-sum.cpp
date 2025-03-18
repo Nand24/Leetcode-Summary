db2fcdb3e3917c93311fb294a9b5ce788cc653da
Pranav Sharma <120562457+Nand24@users.noreply.github.com>
2025-03-18 18:02:49 +0530

    Time: 1 ms (66.80%) | Memory: 13.9 MB (91.26%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -2,23 +2,29 @@ class Solution {
 public:
 void func(int index,vector<vector<int>>&ans,vector<int>&temp,vector<int>&candidates,int target)
 {
-    int sum=0;
-    for(int i=0;i<temp.size();i++)
-    {
-        sum+=temp[i];
-    }
-    if(sum==target)
-    {
-        ans.push_back(temp);
-        return;
-    }
-    if(sum>target) return;
-    for(int i=index;i<candidates.size();i++)
-    {
-            temp.push_back(candidates[i]);
-            func(i,ans,temp,candidates,target);
-            temp.pop_back();
-    }
+  int sum=0;
+  if(index==candidates.size())
+  {
+    return;
+  }
+  for(int i=0;i<temp.size();i++)
+  {
+    sum+=temp[i];
+  }
+  if(sum>target) return;
+  if(sum==target){
+    ans.push_back(temp);
+    return;
+  }
+  // pick
+  temp.push_back(candidates[index]);
+  func(index,ans,temp,candidates,target);
+  
+
+  //not pick
+  temp.pop_back();
+  func(index+1,ans,temp,candidates,target);
+
 
 }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
