ac76993a4285e18e1a0e9509fd5ef5ff290c4a71
Pranav Sharma <120562457+Nand24@users.noreply.github.com>
2025-03-22 10:02:29 +0530

    Time: 8 ms (29.35%) | Memory: 23.7 MB (92.90%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> res;
+        sort(intervals.begin(),intervals.end());
+        int start=intervals[0][0]; int end=intervals[0][1];
+        int i=0;
+        while(i<intervals.size())
+        {
+             // overlap
+                   if(intervals[i][0] <= end)
+                   {
+                    start=min(start,intervals[i][0]);
+                    end=max(end,intervals[i][1]);
+                   }
+               
+                   else{
+                       res.push_back({start,end});
+                    start=intervals[i][0]; end=intervals[i][1];
+                  }
+                  
+                   i++;
+        }
+        res.push_back({start,end});
+        
+        return res;
+    }
+};
