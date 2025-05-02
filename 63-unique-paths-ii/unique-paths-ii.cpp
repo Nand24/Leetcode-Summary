ac7f065b555d2575196d216b93a9efb4ea4fb278
Pranav Sharma <120562457+Nand24@users.noreply.github.com>
2025-05-03 00:07:56 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (28.43%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -6,14 +6,13 @@ int f(int i,int j,vector<vector<int>>&obstacleGrid,int m,int n,vector<vector<int
     {
         return 1;
     }
-    if(i>=m || j>=n ) return 0;
+    if(i>=m || j>=n || obstacleGrid[i][j]==1 ) return 0;
     if(dp[i][j]!=-1) return dp[i][j];
-    int down=0;
-    int right=0;
-    if(i+1<m && obstacleGrid[i+1][j]!=1)
-     down=f(i+1,j,obstacleGrid,m,n,dp);
-    if(j+1<n && obstacleGrid[i][j+1]!=1)
-     right=f(i,j+1,obstacleGrid,m,n,dp);
+
+
+   int down=f(i+1,j,obstacleGrid,m,n,dp);
+
+   int right=f(i,j+1,obstacleGrid,m,n,dp);
 
     return dp[i][j]=down+right;
 
