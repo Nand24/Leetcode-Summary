07322b7304caf23afcaeb4a4c4eff49823fbdc44
Pranav Sharma <120562457+Nand24@users.noreply.github.com>
2025-05-06 19:19:46 +0530

    Time: 43 ms (75.54%) | Memory: 21.1 MB (27.60%) - LeetSync

---
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+int f(int i,int j1,int j2,vector<vector<int>>&grid,int m,int n,vector<vector<vector<int>>>&dp)
+{
+      if(j1<0 || j2<0 || j1>=n || j2>=n)
+      {
+        return -1e9;
+      }
+      if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
+      if(i==m-1)
+      {
+                if(j1==j2)
+                {
+                    return grid[i][j1];
+                }
+                else return grid[i][j1]+grid[i][j2];
+      }
+      // movements of Alice and Bob
+      // for one movement of alice  bob has 3 choices hence 9 possible combinations
+      // j ->{ -1 ,0 ,1}
+      int maxi=-1e9;
+      for(int dj1=-1;dj1<=1;dj1++)
+      {
+        for(int dj2=-1;dj2<=1;dj2++)
+        {
+            if(j1==j2)
+            {
+               maxi=max(maxi,grid[i][j1] + f(i+1,j1+dj1,j2+dj1,grid,m,n,dp));
+            }
+            else
+            {
+                maxi=max(maxi,grid[i][j1] + grid[i][j2] +f(i+1,j1+dj1,j2+dj2,grid,m,n,dp));
+            }
+        }
+      }
+      return dp[i][j1][j2]=maxi;
+}
+    int cherryPickup(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<vector<int>>> dp(m,vector<vector<int>>(n,vector<int>(n,-1)));
+        return f(0,0,n-1,grid,m,n,dp);
+    }
+};
