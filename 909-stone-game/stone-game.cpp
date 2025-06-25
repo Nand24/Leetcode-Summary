e793280bbd882b00d7ed53350c13966d2a01c50f
Pranav Sharma <120562457+Nand24@users.noreply.github.com>
2025-06-26 01:06:53 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (89.46%) - LeetSync

---
+++ 909-stone-game/stone-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        int alice=0;
+        int bob=0;
+        for(int i=0;i<piles.size();i++)
+        {
+            
+
+        }
+        return true;
+    }
+};
