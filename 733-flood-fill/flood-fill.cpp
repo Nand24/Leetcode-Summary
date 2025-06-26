1a7c6fe25ee5e6536fcdd95f5cba2a05b1ffba9c
Pranav Sharma <120562457+Nand24@users.noreply.github.com>
2025-06-26 14:01:52 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (37.43%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-void f(int sr,int sc,int&startcolor,vector<vector<int>>&image,int &color)
-{
-    image[sr][sc]=color;
-    int delrow[]={1,0,0,-1};//DLRU
-    int delcol[]={0,-1,1,0};//DLRU
-    for(int i=0;i<4;i++)
-    {
-        int nr=sr+delrow[i];
-        int nc=sc+delcol[i];
-            if(nr>=0 && nr<image.size()  && nc>=0 && nc<image[0].size()){
-            if(image[nr][nc]==startcolor  && image[nr][nc]!=color )
+void    f(int sr, int sc, int&initialclr, vector<vector<int>>& vis,
+      vector<vector<int>>& image, int &finalclr,vector<vector<int>>& res) {
+        vis[sr][sc] = 1;
+        int di[] = {1, 0, 0, -1};
+        int dj[] = {0, -1, 1, 0};
+        for (int i = 0; i < 4; i++) {
+            int nr=sr+di[i]; int nc=sc+dj[i];
+            if(nr>=0 && nr<image.size() && nc>=0 && nc<image[0].size() && vis[nr][nc]==0 &&image[nr][nc]==initialclr)
             {
-    
-                f(nr,nc,startcolor,image,color);
+                res[nr][nc]=finalclr;
+                f(nr,nc,initialclr,vis,image,finalclr,res);
             }
-            }
-     }
-    
-    return;
-}
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        vector<vector<int>> res;
-        int startcol=image[sr][sc];
-        f(sr,sc,startcol,image,color);
-        return image;
+        }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
+                                  int color) {
+        vector<vector<int>> res = image;
+        int intialclr = image[sr][sc];
+        res[sr][sc]=color;
+        vector<vector<int>> vis = image;
+        for (int i = 0; i < image.size(); i++) {
+            for (int j = 0; j < image[0].size(); j++)
+                vis[i][j] = 0;
+        }
+        f(sr, sc, image[sr][sc], vis, image, color,res);
+        return res;
     }
 };
