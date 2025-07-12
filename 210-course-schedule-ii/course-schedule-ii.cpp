3a23fe74261d5cfe3f02808467db52ee9887ad36
Pranav Sharma <120562457+Nand24@users.noreply.github.com>
2025-07-12 17:39:06 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (78.68%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-        for(auto it: prerequisites)
+vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // code here
+        vector<int> indegree(V,0);
+        vector<vector<int>> adj(V);
+        for(int i=0;i<edges.size();i++)
         {
-            adj[it[1]].push_back(it[0]);
-        }
-        vector<int> indegree(adj.size(),0);
-        for(int i=0;i<adj.size();i++)
-        {
-            for(auto it:adj[i])
-            {
-                indegree[it]++;
-            }
+            int u=edges[i][0];
+            int v=edges[i][1];
+            adj[v].push_back(u);
+            indegree[u]++;
         }
         queue<int> q;
-        for(int i=0;i<indegree.size();i++)
+        vector<int> res;
+        for(int i=0;i<V;i++)
         {
             if(indegree[i]==0) q.push(i);
         }
-        vector<int> res;
         while(!q.empty())
         {
             int node=q.front();
-           q.pop();
-           res.push_back(node);
-           for(auto it:adj[node])
-           {
-            indegree[it]--;
-            if(indegree[it]==0) q.push(it);
-           }
+            q.pop();
+            res.push_back(node);
+            for(auto it: adj[node])
+            {
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
         }
-        if(res.size()==numCourses) return res;
-        return vector<int> ();
+       if(res.size()==V) return res;
+       else return {};
     }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        return topoSort(numCourses,prerequisites);
 
+    }
 };
